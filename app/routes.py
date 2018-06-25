eeb10a24762c0027913f248f32ebf20526794f6a
ArthurMinSein <arthurmin93@gmail.com>
2018-06-25 22:09:13 +0630

    add flask-app

---
+++ app/routes.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template
+from config import Config
+from app.forms import LoginForm
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
+
+if __name__ == '__main__':
+    app.run(debug = True)
